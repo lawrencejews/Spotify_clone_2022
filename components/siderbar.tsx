e23f45fdb973d09cc96375eacaefec40b435799a
lawrencejews <lawrencejews1@gmail.com>
2022-05-02 15:01:35 +0900

    added the navigation

--- components/siderbar.tsx
+++ components/siderbar.tsx
@@ -1,17 +1,124 @@
-import NextImage from 'next/image';
-import { Box, List, ListItem, ListIcon, Divider, Center, LinkOverlay } from '@chakra-ui/layout';
-import { MdHome, MdSearch, MdLibraryMusic, MdPlaylistAdd, MdFavorite } from 'react-icons/md';
+import NextImage from "next/image";
+import NextLink from "next/link";
+import {
+  Box,
+  List,
+  ListItem,
+  ListIcon,
+  Divider,
+  Center,
+  LinkOverlay,
+  LinkBox,
+} from "@chakra-ui/layout";
+import {
+  MdHome,
+  MdSearch,
+  MdLibraryMusic,
+  MdPlaylistAdd,
+  MdFavorite,
+} from "react-icons/md";
+
+// Data mapping for listItems then routing
+const navMenu = [
+  {
+    name: "Home",
+    icon: MdHome,
+    route: "/",
+  },
+  {
+    name: "Search",
+    icon: MdSearch,
+    route: "./search",
+  },
+  {
+    name: "Your Library",
+    icon: MdLibraryMusic,
+    route: "./library",
+  },
+];
+
+const musicMenu = [
+  {
+    name: "Create Playlist",
+    icon: MdPlaylistAdd,
+    route: "/",
+  },
+  {
+    name: "Search",
+    icon: MdSearch,
+    route: "./search",
+  },
+  {
+    name: "Favorites",
+    icon: MdFavorite,
+    route: "./favorite",
+  },
+];
+
+const playlists = new Array(30).fill(1).map((_, i) => `Playlist ${i + 1}`);
 
 const Siderbar = () => {
   return (
-    <Box width="100%" height="calc(100vh - 100px)" bg="black" padding="5px" color="gray">
+    <Box
+      width="100%"
+      height="calc(100vh - 100px)"
+      bg="black"
+      padding="5px"
+      color="gray"
+    >
       <Box paddingY="20px">
         <Box width="120px" marginBottom="20px" paddingX="20px">
           <NextImage src="/logo.svg" height={60} width={120} />
         </Box>
+        <Box marginBottom="20px" />
+        <List spacing={2}>
+          {navMenu.map((menu) => (
+            <ListItem paddingX="20px" fontSize="16px" key={menu.name}>
+              <LinkBox>
+                <NextLink href={menu.route} passHref>
+                  <LinkOverlay>
+                    <ListIcon as={menu.icon} color="white" marginRight="20px" />
+                    {menu.name}
+                  </LinkOverlay>
+                </NextLink>
+              </LinkBox>
+            </ListItem>
+          ))}
+        </List>
+      </Box>
+      <Box marginTop="20px" height="100%">
+        <List spacing={2}>
+          {musicMenu.map((menu) => (
+            <ListItem paddingX="20px" fontSize="16px" key={menu.name}>
+              <LinkBox>
+                <NextLink href={menu.route} passHref>
+                  <LinkOverlay>
+                    <ListIcon as={menu.icon} color="white" marginRight="20px" />
+                    {menu.name}
+                  </LinkOverlay>
+                </NextLink>
+              </LinkBox>
+            </ListItem>
+          ))}
+        </List>
+        <Divider color="gray.800" />
+        {/* scrolling box */}
+        <Box height="66%" overflowY="auto" paddingY="20px">
+          <List spacing={2}>
+            {playlists.map((playlist) => (
+              <ListItem paddingX="20px" key={playlist}>
+                <LinkBox>
+                  <NextLink href="/" passHref>
+                    <LinkOverlay>{playlist}</LinkOverlay>
+                  </NextLink>
+                </LinkBox>
+              </ListItem>
+            ))}
+          </List>
+        </Box>
       </Box>
     </Box>
-  )
+  );
 };
 
 export default Siderbar;
